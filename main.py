a8b8df9833a82a1cbeefa0e2838591e242136902
devfawadraza <fawadraza71@gmail.com>
2025-11-08 18:47:48 +0500

    checking the dataset

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("data/file.csv")
 
-print(df.head(3))
-
-# print(df.info())
-
-# print(df.isnull().sum())
-
-# print(df[df['price'] == 0])
-
-# cols = ['price', 'model', 'mileage', 'engine_capacity', 'vehicle_age']
-# for col in cols:
-#     print(f"\n Rows where {col} == 0")
-#     print (df[df[col] <=10])
-
-
-print("\n Description of the columns:")
-print(df.describe())
+# basic overview
+# print("shape of dataset", df.shape) #rows x columns 
+print("Columns name in dataset", df.columns)
